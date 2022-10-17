08d6a01a6f87ca5f5d5f53491c03b56191203d50
Raumil Dhandhukia <raumild@gmail.com>
2022-10-16 17:40:24 -0700

    Code structure of Facade design pattern.

---
+++ Facade.java
@@ -0,0 +1,61 @@
+public class Facade {
+
+	private int UserType;
+
+	private Product theSelectedProduct;
+
+	private int nProductCategory;
+
+	private ClassProductList theProductList;
+
+	private Person thePerson;
+
+	public boolean login() {
+		return false;
+	}
+
+	public void addTrading() {
+
+	}
+
+	public void vieTrading() {
+
+	}
+
+	public void decideBinding() {
+
+	}
+
+	public void discussBinding() {
+
+	}
+
+	public void submitbinding() {
+
+	}
+
+	public void remind() {
+
+	}
+
+	public void createUser(UserInfoItem userinfoitem) {
+
+	}
+
+	public void createProductList() {
+
+	}
+
+	public void AttachProductToUser() {
+
+	}
+
+	public Product SelectProduct() {
+		return null;
+	}
+
+	public void productOperation() {
+
+	}
+
+}
